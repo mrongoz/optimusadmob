1028d1d9ffb6e3545acf7e69b8541e75c562764d
mrongoz <andremrngz@gmail.com>
2023-09-28 14:15:50 +0800

    Update config.sql

--- Guide/caddy/config.sql
+++ Guide/caddy/config.sql
@@ -1,3 +1,3 @@
-ALTER SYSTEM SET ssl_cert_file TO '/var/lib/postgresql/certs/server.crt';
-ALTER SYSTEM SET ssl_key_file TO '/var/lib/postgresql/certs/server.key';
+ALTER SYSTEM SET ssl_cert_file TO '/var/lib/postgresql/certs/certificate.crt';
+ALTER SYSTEM SET ssl_key_file TO '/var/lib/postgresql/certs/private.key';
 ALTER SYSTEM SET ssl TO 'ON';
